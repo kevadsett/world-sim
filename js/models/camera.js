3a7dbd3a64f72b08fcd31e17dfab4202c8f4633b
Kev Adsett <kev.adsett@gmail.com>
2014-04-18 11:42:51 +0100

    Add more defaults to world model
    Specifically, ones that help with the rendering process

--- js/models/camera.js
+++ js/models/camera.js
@@ -2,8 +2,12 @@ define(function(require) {
     var Camera = function(attributes) {
         this.position = attributes.position || {x: attributes.width/2, y: attributes.height/2};
         if(this.position === undefined) throw new Error('Error: no camera position attributes defined');
+        this.height = attributes.height || this.position.y * 2;
+        this.width = attributes.width || this.position.x * 2;
+        this.rawZoom = 0;
         this.zoomFactor = attributes.zoomFactor || 0;
         this.rotation = attributes.rotation || 0;
+        this.yOffset = (this.zoomFactor * 150 / 20)
 
         console.log("New camera instantiated. position: [" + this.position.x + ", " + this.position.y + "], zoomFactor: " + this.zoomFactor + ", rotation: " + 360 * (this.rotation / Math.PI * 2));
     }
