af431586724ceb401b67e6970ec178e599e72644
Kev Adsett <kev.adsett@gmail.com>
2014-04-18 10:35:06 +0100

    Add minivents library

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
 require.config({
-  baseUrl: "",
-  paths: {
-  },
-  waitSeconds: 15
+  	baseUrl: "",
+  	paths: {
+  	},
+  	waitSeconds: 15
 });
 
-require( ["js/models/world.js", "js/views/world.js", "js/models/camera.js", "js/controllers/userinput.js"],
-  function(WorldModel, WorldView, Camera, UserInput) {
-      var worldModel = new WorldModel(128);
-      new Camera(800, 600, {rotation:Math.PI});
-      new UserInput();
-      new WorldView(worldModel);
-  }
+require( [
+	"js/models/world.js", 
+	"js/views/world.js", 
+	"js/controllers/camera.js", 
+	"js/controllers/userinput.js",
+	"js/libs/minivents"
+	],
+  	function(WorldModel, WorldView, Camera, UserInput, Events) {
+      	window.worldEvents = new Events();
+      	var worldModel = new WorldModel();
+      	new Camera({width:800, height:600, rotation:Math.PI});
+      	new UserInput();
+      	new WorldView(worldModel);
+  	}
 );
